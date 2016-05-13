e263427877c2f38c7aca01b7fc735308211212c5
yuanning <727665171@qq.com>
2016-05-13 18:02:56 +0800

    login

--- dotamax_spider/spiders/test2.py
+++ dotamax_spider/spiders/test2.py
@@ -1,3 +1,33 @@
-import datetime
+import requests, sys
 
-print type(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+login_url = 'http://192.168.1.88/bugfree/index.php/site/login'
+home_url_2 = 'http://192.168.1.88/bugfree/index.php/bug/list/2'
+
+login_cookie = '33_product=d5e0121d62d81c633347eb9cb1dcfb4e8c8e11e9s%3A1%3A%223%22%3B; ' \
+               'pageSize=4a634a46b3691d708a05ba276841dd25c535eea8s%3A2%3A%2220%22%3B; ' \
+               'PHPSESSID=v3o1i0sl8kmt39misesog43ab2; ' \
+               'language=2470c92f0603e5fb7db237fcf3ccbf0cce076868s%3A5%3A%22zh_cn%22%3B'
+login_headers = {}
+login_headers['Host'] = '192.168.1.88'
+login_headers['User-Agent'] = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:31.0) Gecko/20100101 Firefox/31.0"
+login_headers['Referer'] = "http://192.168.1.88/bugfree/index.php/site/login"
+login_headers['Cookie'] = login_cookie
+
+login_data = 'LoginForm%5Busername%5D=yuanning&LoginForm%5Bpassword%5D=123456&LoginForm%5Blanguage%5D=zh_cn&LoginForm%5BrememberMe%5D=0'
+login_data_dict = {}
+login_data_dict['LoginForm[username]']="yuanning"
+login_data_dict['LoginForm[password]']="123456"
+login_data_dict['LoginForm[language]']="zh_Cn"
+login_data_dict['LoginForm[rememberMe]']="0"
+
+login_session = requests.session()
+print login_session.cookies.get_dict()
+login_session.post(login_url, login_data_dict)
+print login_session.cookies.get_dict()
+print login_session.get(home_url_2).url
+
+with open('./bugfree.txt', 'wb') as f:
+    f.write(requests.get(home_url_2, cookies=login_session.cookies.get_dict()).content.decode('utf-8'))
---
+++ dotamax_spider/spiders/test3.py
@@ -0,0 +1,26 @@
+import requests, sys
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+LOGIN_URL = 'https://testserver.comein.cn/ComeinManager/loginView'
+HOME_URL = 'https://testserver.comein.cn/ComeinManager/'
+
+login_data = {}
+login_data['key'] = '888e707a0e5a171a037f82ecaad3d800fd6981233792d4417' \
+                    'c2825cf12e11d68e80cd33d50c5ac4d8fd30f6328a52d82f76' \
+                    'bb659d9de50eae1b2f3efc240b6fc7c8685c35c1d0fad4582ef' \
+                    '2101b99a10e14b740625a32a396ab637dae230ee627d23f97df6' \
+                    '587c890c4429674181a546fe06a91ed5ea724f3020bdaea72475e8'
+my_cookie = {}
+my_cookie['JSESSIONID'] = '81444267-8da8-46c2-ab50-fbd0a4f0a071'
+# my_cookie['rememberMe'] = 'fgiqQiitOcrZLxVsJWftwyiUuahxH03Wvkhb63NI69wRLT4JK3divg8AheLqb0Ryvvl94eUPwMJaN792FFXC1HpQYl50bXJd9DDYaKNqneJIZn+o60jNTjQYoX09TArVG/0lEYXVHgxBhBn6olskBe4DmALrUehWSgUPElXzB5CJ9UIzQcmpUeZUbiaMgeG5urKFRMLoEEu95YqIlEKr/PlVZlc6cQixuW/TiZvQn0iCFU4wPU1G2/YwNm3flL2upvWzmcSHmIfL9qjpyGfv16M9qkS1QSdcpKi7/BNxMXyJHcThmxxdouY1o3JZnLTC9vO3WdmncCYeY3E9ORvq0qp1WZea6CxSDggVQeOZiwU3BWQpi0hb5s33tdSi6b2ZHOoZ7KJf19CyoHNhSvetk5WJsJJuJi0KUiCIw4NlGEr9m++XvAbRS9WXOsZmFQilXtwND8Y8fY7AwtQEKpvpVWrCU/AmCE9xBw1HSMIZrh2sMRxqBNCeau29s84u87x6'
+
+login_session = requests.session()
+ls = login_session.post(LOGIN_URL, login_data)
+print ls.cookies.get_dict()
+print login_session.get(HOME_URL).url
+print requests.get(HOME_URL, cookies=my_cookie).url
+
+with open('./testserver.txt', 'wb') as f:
+    f.write(requests.get(HOME_URL, cookies=my_cookie).text.encode('utf-8'))
